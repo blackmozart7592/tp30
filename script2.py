3c061a65179641e7e2003534c3444c084d4eb260
bguisse99 <114409062+bguisse99@users.noreply.github.com>
2022-09-29 16:30:36 +0200

    Create script2.py

---
+++ script2.py
@@ -0,0 +1,10 @@
+#Lire la saisie de l'utilisateur
+min = int(1)
+max = int(1000000)
+for n in range(min,max + 1):
+   if n > 1:
+       for i in range(2,n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)
